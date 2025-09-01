4ad83c71154e135b6312703daf0b708ddb390ce6
ECOWWWWWWWW <jamesjerico.aquino@lorma.edu>
2025-09-01 14:30:52 +0900

    Tradie Seeder Modified

---
+++ routes/api.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\Api\Auth\HomeownerAuthController;
+use App\Http\Controllers\Api\Auth\TradieAuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Homeowner Authentication Routes
+Route::prefix('homeowner')->group(function () {
+    Route::post('register', [HomeownerAuthController::class, 'register']);
+    Route::post('login', [HomeownerAuthController::class, 'login']);
+    
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [HomeownerAuthController::class, 'logout']);
+        Route::get('me', [HomeownerAuthController::class, 'me']);
+    });
+});
+
+// Tradie Authentication Routes
+Route::prefix('tradie')->group(function () {
+    Route::post('register', [TradieAuthController::class, 'register']);
+    Route::post('login', [TradieAuthController::class, 'login']);
+    
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [TradieAuthController::class, 'logout']);
+        Route::get('me', [TradieAuthController::class, 'me']);
+    });
+});
+
+// Protected routes for authenticated users
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
