bb67ab2428c56369f56fb959550af276957c52a2
manofsteeljj <justinjoseph.sanchez@lorma.edu>
2025-09-01 14:14:48 +0800

    first

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Justin Joseph Sanchez',
-            'email' => 'admin@gmail.com',
+            'email' => 'justin@gmail.com',
             'password' => Hash::make("admin"),
         ]);
 
