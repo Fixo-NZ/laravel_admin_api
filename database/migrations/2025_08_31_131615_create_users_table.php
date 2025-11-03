639f94678a203cadf47aa7e4979de55a5ee071ce
ElijahDelaRosa <elijahgarrence.delarosa@lorma.edu>
2025-11-03 11:11:40 +0800

    short db fix

--- database/migrations/2025_08_31_131615_create_users_table.php
+++ database/migrations/2025_08_31_131615_create_users_table.php
@@ -25,8 +25,6 @@ return new class extends Migration
             $table->string('location')->nullable();
             $table->rememberToken();
             $table->timestamps();
-            $table->string('phone')->nullable();
-            $table->string('location')->nullable();
 
         });
     }
